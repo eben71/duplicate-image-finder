6d99a5dbbf92c5c2c7e205cb4251719eaa6320f4
Eben Venter <eben.venter@gmail.com>
2025-05-19 21:10:24 +0800

    fixing linting

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -8,6 +8,7 @@ from sqlmodel import SQLModel
 from alembic import context
 from sqlalchemy import create_engine, pool
 from logging.config import fileConfig
+from typing import cast 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -29,12 +30,12 @@ target_metadata = SQLModel.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
-from backend.config.settings import settings
-_env_db_url = os.getenv("DATABASE_URL", settings.database_url)
-if _env_db_url is None:
-    raise RuntimeError("DATABASE_URL is required")
+_db_url_env = os.getenv("DATABASE_URL")
+if _db_url_env is None:
+    from backend.config.settings import settings
+    _db_url_env = settings.database_url
 
-db_url: str = _env_db_url  
+db_url: str = cast(str, _db_url_env) 
 config.set_main_option("sqlalchemy.url", db_url)
 
 def run_migrations_offline() -> None:
