ba4e5c8fa38b9daec672b66af5c190ba5213c599
Eben Venter <eben.venter@gmail.com>
2025-07-28 23:23:36 +0800

    Fix session return type

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,9 +1,10 @@
 from sqlmodel import create_engine, Session
 from backend.config.settings import settings
+from typing import Generator
 
 engine = create_engine(settings.DATABASE_URL, echo=True)
 
 
-def get_session() -> Session:
+def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
