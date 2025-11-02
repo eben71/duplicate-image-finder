59334a3333c8efa29e47e51f3edab76058ec00dc
Eben Venter <eben.venter@gmail.com>
2025-11-02 19:30:33 +0800

    More changes

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from "tailwindcss";
 
-export default {
-  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./pages/**/*.{ts,tsx}"],
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}"
+  ],
   theme: {
     extend: {
       colors: {
@@ -29,4 +33,6 @@ export default {
     }
   },
   plugins: []
-} satisfies Config;
+};
+
+export default config;
